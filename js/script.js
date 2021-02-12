326f6ccdee0d19939a54aadf91e3424fb21a62d4
nekrorat <nekrorat@gmail.com>
2021-02-12 08:27:29 +0200

    The first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let isChecked = true,
+    isClose = false;
+
+console.log('console log command');
+
+//alert('alert command');
+
+let answer = prompt('are u 18 years old?', 'Yes');
+
+console.log(typeof(null));
